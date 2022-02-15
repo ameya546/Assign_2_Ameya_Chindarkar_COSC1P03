16fa48aaf12c93bdd8ed782d26bbc4e567ee3f13
ameya546 <99606138+ameya546@users.noreply.github.com>
2022-02-15 10:07:42 -0500

    IV code

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Assign_2_Ameya_Chindarkar_COSC1P03
- 
+1. createLinkedList(string text)
+
+2. insert(int l, int s, string text)
+        if ( s is null or s<0)
+            insertLine(int l, string text)
+        else
+            insertText(int l, int s, string text)
+3. insertLine(int l, string text)
+           i. create linked list for text, call method
+           ii. check Node[l], if its not null then 
+                             move all lines starting from pos l down
+           iv.  add linked list to pos l
+4. insertText(int l, int s, string text)
+    i.check if s ==0 then append two linkedlist and change head
+ii. loop, till s reached or end of linked list, with index i
+iii. check i == s then, insert in between
+iv.else insert at end
--- src/IV/IvEditorApp.java
+++ src/IV/IvEditorApp.java
@@ -4,13 +4,92 @@ import BasicIO.BasicForm;
 public class IvEditorApp {
     Node [] lineHeads = new Node[50];
 
+    private  Node createLine (String str) {
+        Node dummy = new Node(' ');
+
+        if(str == null || str.length() == 0) {
+            return dummy.next;
+        }
+
+        Node prev = dummy;
+
+        for(char c: str.toCharArray()){
+            prev.next = new Node(c);
+            prev = prev.next;
+        }
+
+        return dummy.next;
+
+    }
+
+    private void  insert(int ipLine, int ipStart, String ipText){
+        Node lineHead = createLine(ipText);
+
+        if(ipStart < 0){
+            insertLine(ipLine, lineHead);
+        } else {
+            insertText(ipLine, ipStart, lineHead);
+        }
+    }
+
+    private void insertLine(int ipLine, Node lineHead){
+        if(lineHeads[ipLine] == null) {
+            lineHeads[ipLine] = lineHead;
+        } else {
+            Node prev = lineHeads[ipLine];
+            Node curr = lineHeads[ipLine + 1];
+
+            for(int i = ipLine +1; i < lineHeads.length; i++){
+                curr = lineHeads[i];
+                lineHeads[i] = prev;
+                prev = curr;
+            }
+
+            lineHeads[ipLine] = lineHead;
+        }
+    }
+
+    private void insertText(int ipLine, int ipStart, Node lineHead){
+            System.out.println("setting txt");
+            if (ipStart == 0) {
+
+
+            }else if (ipStart > 0){
+                for (int i, i == ipStart)
+
+            }
+
+
+    }
+
+
+    private void writeOutput(BasicForm form) {
+        StringBuilder output = new StringBuilder();
+
+        for(Node lineHead: lineHeads){
+            while(lineHead != null){
+                System.out.println(lineHead.data);
+                output.append(lineHead.data);
+                lineHead = lineHead.next;
+            }
+            output.append("\r\n");
+        }
+
+        form.clear("O");
+        form.writeString("O", output.toString());
+
+    }
+
     public static void main(String[] args){
         IvEditorApp app = new IvEditorApp();
 
-        BasicForm form =app.createForm()
+        BasicForm form =app.createForm();
 
         int ipLine;
-        String ipStart;
+        int ipStart;
+        int ipEnd;
+        String ipText;
+        String ipAction;
 
 
 
@@ -18,16 +97,27 @@ public class IvEditorApp {
             form.accept();
             //1. read inputs
             ipLine = form.readInt("L");
-            System.out.println(form.readString("S"));
-            System.out.println(form.readString("E"));
-            System.out.println(form.readString("T"));
-            System.out.println(form.readString("action"));
+            ipStart = form.readInt("S");
+            System.out.println("start : " + ipStart);
+            ipEnd = form.readInt("E");
+            ipText = form.readString("T");
+            ipAction = form.readString("action");
+
 
             //2. check action and process
             if(ipAction.equals("Insert")){
+                System.out.println(ipAction);
+                app.insert(ipLine, ipStart, ipText);
 
-            } else if
-            form.writeString("O" , form.readString("T"));
+            }else if (ipAction.equals("Delete")){
+                System.out.println(ipAction);
+
+            }else if (ipAction.equals("Replace")){
+                System.out.println(ipAction);
+            }
+
+            //form.writeString("O" , form.readString("T"));
+            app.writeOutput(form);
         }while(true);
    }
 
